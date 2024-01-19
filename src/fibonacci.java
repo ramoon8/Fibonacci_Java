54d4fb8555b2f5aeb68b40e159784c3e4628dfd7
mahdi hedayati <153181720+ramoon8@users.noreply.github.com>
2024-01-20 01:17:14 +0330

    Add files via upload

---
+++ src/fibonacci.java
@@ -0,0 +1,22 @@
+public class fibonacci {
+    public static void main(String[] args) {
+        int n = 10; // تعداد اعضا
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
+
+      
+        
+
